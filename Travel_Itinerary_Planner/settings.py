0beb2da98885e201dec9a03744f66432f438dc8b
Anji Reddy <godalaanjireddy16@gmail.com>
2026-03-27 01:39:56 +0530

    fix deploy

--- Travel_Itinerary_Planner/settings.py
+++ Travel_Itinerary_Planner/settings.py
@@ -126,3 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ALLOWED_HOSTS = ['*']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
